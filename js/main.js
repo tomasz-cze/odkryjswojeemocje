1bb0dd425614e7a505f04b988d7e3a1150491621
unknown <tomasz.czechlewski@gmail.com>
2022-01-16 11:58:08 +0100

    added new functions to navigation in JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar-collapse')
+	const navHamburger = document.querySelector('.fa-bars')
 	const allNavItems = document.querySelectorAll('.nav-link')
 	const navList = document.querySelector('.collapse')
 
@@ -10,10 +11,14 @@ document.addEventListener('DOMContentLoaded', function () {
 			nav.classList.remove('shadow-bg')
 		}
 	}
+	navHamburger.addEventListener('click', () => navList.classList.remove('show'))
 
 	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+	navHamburger.addEventListener('click', () => nav.classList.add('shadow-bg'))
 
 	const articules = document.querySelectorAll('.blog-card')
 
 	window.addEventListener('scroll', addShadow);
+
 })
